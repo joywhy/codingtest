19f39fede26ea1b2b9dc77d789524634f7ca1a8e
joywhy <82435813+joywhy@users.noreply.github.com>
2023-04-13 00:49:58 +0900

    [level 1] Title: 폰켓몬, Time: 0.82 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,7 +1,9 @@
 function solution(nums) {
  let newarr= [];
     nums.map((x,idx)=>{
-        if(idx=0||newarr.indexOf(x)===-1){
+        if(idx=0||newarr.indexOf(x)===-1){ 
+            //indexOf 배열의 앞쪽부터 비교해서 차례 나감으로 해시로 푼것이 아님
+            //
             newarr.push(x); 
         }
     });
