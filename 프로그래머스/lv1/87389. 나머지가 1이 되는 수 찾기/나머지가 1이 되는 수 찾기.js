5556da2ae2a80718cdc3edc59f6c0ad7831ed134
EunHee Lee <82435813+joywhy@users.noreply.github.com>
2023-09-08 19:55:25 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = 0;
+    const num = n-1;
+    for(let i =2; i<=num; i++){
+        if(num%i===0){
+            return i;
+        }
+        
+    }
+   
+   
+}
