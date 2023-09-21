79a766ec83ad35ef2d7d874ea4e0681ad3332e82
EunHee Lee <82435813+joywhy@users.noreply.github.com>
2023-09-21 16:29:15 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,11 @@
+function solution(seoul) {
+    let idx =0; 
+for(let i = 0 ; i<seoul.length; i++){
+    if(seoul[i]==="Kim"){
+        idx= i;
+     break;
+    }
+}
+    return `김서방은 ${idx}에 있다`;
+    
+}
