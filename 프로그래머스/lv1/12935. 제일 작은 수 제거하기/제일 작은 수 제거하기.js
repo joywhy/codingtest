2b26e34a19975838e07ade8f60514c2f270d0983
EunHee Lee <82435813+joywhy@users.noreply.github.com>
2023-09-27 14:11:46 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 4.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,16 +1,22 @@
 function solution(arr) {
-    if(arr.length===1){
-        return [-1];
-    }
-    let el = [arr[0],0];
-   arr.forEach((ell,idx)=>{
-       if(el[0]>ell){
-           el = [ell,idx];
-       }
-   })
-    const answer = arr.filter((ell,idx)=>{
-        return ell!==el[0];
-    });
+//     if(arr.length===1){
+//         return [-1];
+//     }
+//     let el = [arr[0],0];
+//    arr.forEach((ell,idx)=>{
+//        if(el[0]>ell){
+//            el = [ell,idx];
+//        }
+//    })
+//     const answer = arr.filter((ell,idx)=>{
+//         return ell!==el[0];
+//     });
 
-    return answer;
+//     return answer;
+    let arrr = [1,1,2,3,1,4]
+    arrr.splice(1,1);
+    console.log(arrr);
+      arr.splice(arr.indexOf(Math.min(...arr)),1);
+    if(arr.length<1)return[-1];
+    return arr;
 }
