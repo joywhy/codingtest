f0806679165b0923e852198779b0014d5b306571
EunHee Lee <82435813+joywhy@users.noreply.github.com>
2023-12-18 15:14:03 +0900

    [level 2] Title: 기능개발, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,31 +1,29 @@
 function solution(progresses, speeds) {
-    let deadline = 0;
-    let count = 0; 
+    let releaseDate = 0;
+    let features = 0; 
     
     let dueDate = progresses.map((el,idx)=>{
      let task = (100-el)/speeds[idx];              
      return  Math.ceil(task);
     });
    
-    console.log(dueDate);
     let answer =[];
      dueDate.forEach((el,idx,arr)=>{
-        //  console.log(deadline);
-        // console.log(count);
-          if(deadline<el){
-              deadline = el;  
+    
+          if(releaseDate<el){
+              releaseDate = el;  
            }
-          if(deadline>=el){
-            count=count+1;
+          if(releaseDate>=el){
+            features=features+1;
            }
-           if((deadline>=el)&&(deadline<arr[idx+1])){
-             let dat=count;
-             count=0;
-               console.log(dat);
+           if((releaseDate>=el)&&(releaseDate<arr[idx+1])){
+             let dat=features;
+             features=0;
+     
             answer.push(dat);   
            }
          if(idx === arr.length-1){
-            return  answer.push(count);  
+            return  answer.push(features);  
          }
     });
     return answer;
